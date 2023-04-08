223a03dc18c5fe8a3e6fa5483f9eea3b523ca12c
scottdeutsch40 <scottdeutsch40@gmail.com>
2023-04-08 15:14:10 -0400

    Finished Holdings_Retrieval

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+const holdingsController = require('../controllers/holdingsController.js');
+
 // router.get('/login', loginController);
 // create new user (DB)
 // authenticate user (DB)
 
 // My portfolio
 // get users stocks/qty (DB)
+router.get('/holdings/:id', holdingsController.getHoldings, (req, res) => {
+    res.status(200).send(res.locals.holdings);
+  })
 // post/patch/delete updated qtys (DB)
 // get current closing price for all ticker symbols (API)
 
