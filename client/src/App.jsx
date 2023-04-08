959adc43125c2aa30e5a1436bbcfe534f6ebd603
Ted Chu <tchu002@gmail.com>
2023-04-08 14:02:46 -0700

    login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -5,19 +7,34 @@ import './App.css';
 import './scss/main.scss';
 import Home from './components/pages/Home.jsx';
 import Navbar from './components/Navbar';
+import Login from './components/pages/Login.jsx';
+import  { Toaster } from 'react-hot-toast';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
-	return (
-		<>
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-				</Routes>
-			</Router>
-			<ToastContainer />
-		</>
-	);
+  const [user, setUser] = useState( {firstName: "", lastName:"", email: "", password: ""});
+  const login = userData => {
+		setUser(
+      {firstName: userData.firstName, lastname: userData.lastName, email: userData.email, password: userData.password})
+  }
+  console.log(user)
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login login={login} />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
+
+
