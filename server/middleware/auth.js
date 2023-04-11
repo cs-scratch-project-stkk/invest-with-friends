279d4f9d04a4b9b768165fc8f54ca61ad3c4dde8
Kevin <kevinyoon01@gmail.com>
2023-04-10 18:47:36 -0700

    Server.js userController file updated with userData obj

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,21 +2,20 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/config');
 
 const checkAuth = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
+	try {
+		const token = req.headers.authorization.split(' ')[1];
 
-    if (!token) {
-      throw new Error('Authentication failed');
-    }
+		if (!token) {
+			throw new Error('Authentication failed');
+		}
 
-    const payload = jwt.verify(token, JWT_SECRET);
-    console.log(payload);
-    res.locals.userId = payload.id
-  
-    return next();
-  } catch (err) {
-    return next(err);
-  }
-}
+		const payload = jwt.verify(token, JWT_SECRET);
+		res.locals.userId = payload.id;
+
+		return next();
+	} catch (err) {
+		return next(err);
+	}
+};
 
 module.exports = checkAuth;
