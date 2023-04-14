5f182dbd73fe138bc53523bfaf8f5e1fa96227a6
Kevin-Featherstone <117868278+Kevin-Featherstone@users.noreply.github.com>
2023-04-14 13:47:19 -0500

    Refactor error message in addHolding validator

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,28 +1,28 @@
 const validator = {};
 
 validator.addHolding = (req, res, next) => {
-  const { user_id, ticker, shares } = req.body
+	const { user_id, ticker, stock_quantity } = req.body.stockData;
 
-  if (typeof user_id === 'number' && typeof ticker === 'string' && typeof shares === 'number') {
-    return next()
-  }
-  else {
-    return next(`
+	if (typeof user_id === 'number' && typeof ticker === 'string' && typeof stock_quantity === 'number') {
+		return next();
+	} else {
+		return next(`
       error: req.body
       expected: 
         {
           user_id: <number>,
           ticker: <string>,
-          shares: <number>,
+          stock_quantity: <number>,
         }
       received:
         {
           user_id: ${user_id} <${typeof user_id}>,   
           ticker: ${ticker} <${typeof ticker}>,
-          shares: ${shares} <${typeof shares}>,
+          stock_quantity: ${stock_quantity} <${typeof stock_quantity}>,
         }
-    `)
-  }
+      req.body: ${JSON.stringify(req.body)}
+    `);
+	}
 };
 
 module.exports = validator;
