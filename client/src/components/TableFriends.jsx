c368195e11c656ad1a24cb1386d55bc05c9d96a6
Kevin <kevinyoon01@gmail.com>
2023-04-14 17:11:21 -0700

    Friends List in progress

--- client/src/components/TableFriends.jsx
+++ client/src/components/TableFriends.jsx
@@ -18,51 +18,51 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 
 function TableFriends({ friends }) {
-	const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-		'& .MuiDialogContent-root': {
-			padding: theme.spacing(2),
-		},
-		'& .MuiDialogActions-root': {
-			padding: theme.spacing(1),
-		},
-	}));
+	// const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+	// 	'& .MuiDialogContent-root': {
+	// 		padding: theme.spacing(2),
+	// 	},
+	// 	'& .MuiDialogActions-root': {
+	// 		padding: theme.spacing(1),
+	// 	},
+	// }));
 
-	function BootstrapDialogTitle(props) {
-		const { children, onClose, ...other } = props;
+	// function BootstrapDialogTitle(props) {
+	// 	const { children, onClose, ...other } = props;
 
-		return (
-			<DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-				{children}
-				{onClose ? (
-					<IconButton
-						aria-label="close"
-						onClick={onClose}
-						sx={{
-							position: 'absolute',
-							right: 8,
-							top: 8,
-							color: (theme) => theme.palette.grey[500],
-						}}>
-						<CloseIcon />
-					</IconButton>
-				) : null}
-			</DialogTitle>
-		);
-	}
+	// 	return (
+	// 		<DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+	// 			{children}
+	// 			{onClose ? (
+	// 				<IconButton
+	// 					aria-label="close"
+	// 					onClick={onClose}
+	// 					sx={{
+	// 						position: 'absolute',
+	// 						right: 8,
+	// 						top: 8,
+	// 						color: (theme) => theme.palette.grey[500],
+	// 					}}>
+	// 					<CloseIcon />
+	// 				</IconButton>
+	// 			) : null}
+	// 		</DialogTitle>
+	// 	);
+	// }
 
-	BootstrapDialogTitle.propTypes = {
-		children: PropTypes.node,
-		onClose: PropTypes.func.isRequired,
-	};
+	// BootstrapDialogTitle.propTypes = {
+	// 	children: PropTypes.node,
+	// 	onClose: PropTypes.func.isRequired,
+	// };
 
-	const [open, setOpen] = React.useState(false);
+	// const [open, setOpen] = React.useState(false);
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
-	const handleClose = () => {
-		setOpen(false);
-	};
+	// const handleClickOpen = () => {
+	// 	setOpen(true);
+	// };
+	// const handleClose = () => {
+	// 	setOpen(false);
+	// };
 
 	return (
 		<>
