8a5709f03e6be732cfe765082accebe1a63f0b20
Kevin-Featherstone <117868278+Kevin-Featherstone@users.noreply.github.com>
2023-04-14 19:17:32 -0500

    Add relationships request validation middleware

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,8 +1,7 @@
 const validator = {};
 
 validator.addHolding = (req, res, next) => {
-	const { user_id, ticker, stock_quantity } = req.body.stockData;
-
+	const { user_id, ticker, stock_quantity } = req.body;
 	if (typeof user_id === 'number' && typeof ticker === 'string' && typeof stock_quantity === 'number') {
 		return next();
 	} else {
@@ -24,5 +23,51 @@ validator.addHolding = (req, res, next) => {
     `);
 	}
 };
+
+validator.deleteHolding = (req, res, next) => {
+	const { user_id, ticker } = req.body;
+	if (typeof user_id === 'number' && typeof ticker === 'string') {
+		return next();
+	} else {
+		return next(`
+      error: req.body
+      expected: 
+        {
+          user_id: <number>,
+          ticker: <string>,
+        }
+      received:
+        {
+          user_id: ${user_id} <${typeof user_id}>,   
+          ticker: ${ticker} <${typeof ticker}>,
+        }
+      req.body: ${JSON.stringify(req.body)}
+    `);
+	}
+};
+
+validator.relationships = (req, res, next) => {
+  const { user_id, first_name, last_name } = req.body;
+	if (typeof user_id === 'number' && typeof ticker === 'string') {
+		return next();
+	} else {
+		return next(`
+      error: req.body
+      expected: 
+        {
+          user_id: <number>,
+          first_name: <string>,
+          last_name: <string>,
+        }
+      received:
+        {
+          user_id: ${user_id} <${typeof user_id}>,   
+          first_name: ${first_name} <${typeof first_name}>,
+          last_name: ${last_name} <${typeof last_name}>,
+        }
+      req.body: ${JSON.stringify(req.body)}
+    `);
+	}
+}
 
 module.exports = validator;
